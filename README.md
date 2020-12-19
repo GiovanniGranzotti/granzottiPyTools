8954d1a839230675715308ccd53a0c0254d551a7
Giovanni <giogranzotti@hotmail.com>
2020-12-19 18:37:57 -0300

    Isolamento de testes.

--- README.md
+++ README.md
@@ -65,3 +65,4 @@ pip install -i https://test.pypi.org/simple/ libgranzottiPyTools
 * no dia **10/12/2020** Produção de código testável.
 * no dia **12/12/2020** Injeção de dependências.
 * no dia **14/12/2020** Uso do Mock.
+* no dia **19/12/2020** Aprendi a isolar testes
--- libgranzottiPyTools/tests/test_github_api.py
+++ libgranzottiPyTools/tests/test_github_api.py
@@ -1,14 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libgranzottiPyTools import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars3.githubusercontent.com/u/101380?v=4'
     resp_mock.json.return_value = {
         'login': 'giovanni', 'id': 101380,
-        'avatar_url': 'https://avatars3.githubusercontent.com/u/101380?v=4'
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('giovanni')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('giovanni')
     assert 'https://avatars3.githubusercontent.com/u/101380?v=4' == url
